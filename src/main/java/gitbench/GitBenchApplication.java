ea50ff9c7710d9120d8a96e9dca361af41dd250d
Nepeli Mumladze <mumladzen@bog.ge>
2025-12-29 23:11:31 +0400

    Spring Boot

---
+++ src/main/java/gitbench/GitBenchApplication.java
@@ -0,0 +1,11 @@
+package gitbench;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitBenchApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GitBenchApplication.class, args);
+	}
+}
---
+++ src/test/java/gitbench/GitBenchApplicationTests.java
@@ -0,0 +1,13 @@
+package gitbench;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitBenchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
