4cfa0908b18eeebbf0f66f0982f6b1c710d48ca6
Nepeli Mumladze <mumladzen@bog.ge>
2025-12-30 00:13:12 +0400

    Organize modules, add spring boot application annotations

---
+++ gitbench-cli/src/main/java/cli/GitBenchCliApplication.java
@@ -0,0 +1,11 @@
+package cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitBenchCliApplication {
+    static void main(String[] args) {
+        SpringApplication.run(GitBenchCliApplication.class, args);
+    }
+}
---
+++ gitbench-web/src/main/java/web/GitBenchWebApplication.java
@@ -0,0 +1,11 @@
+package web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitBenchWebApplication {
+    static void main(String[] args) {
+        SpringApplication.run(GitBenchWebApplication.class, args);
+    }
+}
